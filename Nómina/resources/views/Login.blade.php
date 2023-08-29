89aec33424559de5503bdf9854ebe98c7f2e0e02
Jhonmarco Morillo <bc219132@Bancaribe.com.ve>
2023-08-29 15:17:31 -0400

    Vista Login
    Vista del ingreso, para ingresar al Sistema

--- Nómina/resources/views/Login.blade.php
+++ Nómina/resources/views/Login.blade.php
@@ -1,52 +1,82 @@
-<div class="vh-100 d-flex align-items-center">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-5 col-md-6 col-sm-8 col-xs-10 mx-auto">
-          <div id="loginFrame" class="position-relative">
-            <div id="signup" class="levelDown shadow bg-white card p-3">
-              <h3 class="my-2">Signup</h3>
-              <form action="" method="" onsubmit="" class='mx-3 my-3'>
-                <div class="mb-3">
-                  <label for="fullname">Full Name</label>
-                  <input type="text" class="mt-2 form-control" id="fullname" name="fullname">
-                </div>
-                <div class="mb-3">
-                  <label for="email">Email</label>
-                  <input type="email" class="mt-2 form-control" id="email" name="email">
-                </div>
-                <div class="mb-4">
-                  <label for="password">Password</label>
-                  <input type="password" class="mt-2 form-control" id="password" name="password">
-                </div>
-                <div class="mb-2">
-                  <button class="btn btn-lg btn-primary w-100">Login</button>
-                </div>
-              </form>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Login</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="{{ asset('assets/Style_Login.css')}}">
+
+</head>
+
+<body>
+ 
+  <section class="vh-100">
+    <div class="container-fluid h-custom">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-md-9 col-lg-6 col-xl-5">
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+            class="img-fluid" alt="Sample image">
+        </div>
+        <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+          <form>
+            <H1>Sistema de Nómina</H1>
+            <div class="divider d-flex align-items-center my-4">
+              <p class="text-center fw-bold mx-3 mb-0">Registro de credenciales</p>
             </div>
-            <div id="login" class="levelUp shadow bg-white card p-3">
-              <h3 class="my-2">Login</h3>
-              <form action="" method="" onsubmit="" class='mx-3 my-3'>
-                <div class="mb-3">
-                  <label for="email">Email</label>
-                  <input type="email" class="mt-2 form-control" id="email" name="email">
-                </div>
-                <div class="mb-2">
-                  <label for="password">Password</label>
-                  <input type="password" class="mt-2 form-control" id="password" name="password">
-                </div>
-                <div class="mb-2">
-                  <input type="checkbox" class="me-2" for="rememeberMe"> <label for="rememberMe">Remember Me</label>
-                </div>
-                <div class="mb-2">
-                  <button class="btn btn-lg btn-success w-100">Login</button>
-                </div>
-                <div class="mt-4 text-center">
-                  <a href="">Forgot Password?</a>
-                </div>
-              </form>
+  
+            <!-- Email input -->
+            <div class="form-outline mb-4">
+              <input type="text" name="Nombre_Usuario" id="form3Example3" class="form-control form-control-lg"
+                placeholder="Usuario"/>
+              <label class="form-label" for="form3Example3">Usuario Registrado</label>
             </div>
-          </div>
+  
+            <!-- Password input -->
+            <div class="form-outline mb-3">
+              <input type="password" name="Contraseña" id="form3Example4" class="form-control form-control-lg"
+                placeholder="Contraseña" />
+              <label class="form-label" for="form3Example4">Contraseña</label>
+            </div>
+  
+            <div class="d-flex justify-content-between align-items-center">
+              <!-- Checkbox -->
+              <div class="form-check mb-0">
+                <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+                <label class="form-check-label" for="form2Example3">
+                  Recordar
+                </label>
+              </div>
+             
+            </div>
+  
+            <div class="text-center text-lg-start mt-4 pt-2">
+              <button type="button" class="btn btn-primary btn-lg"
+                style="padding-left: 2.5rem; padding-right: 2.5rem;">Iniciar sesión</button>
+          
+            </div>
+  
+          </form>
         </div>
       </div>
     </div>
-  </div>
+    
+  </section>
+
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
